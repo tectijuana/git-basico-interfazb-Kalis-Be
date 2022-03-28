3912c752c04447f326ae1e3dc351c72047afa502
Claudia A. BENITEZ <99373782+Kalis-Be@users.noreply.github.com>
2022-03-28 08:29:28 -0700

    Add files via upload

--- capitulo1programa56.cpp
+++ capitulo1programa56.cpp
@@ -1,11 +1,28 @@
-/*Se dis[pone una lista que contiene los examenes 
-de grado finales para una clase de 20 alumnos.Contar las calificaciones 
-abajo de 65 e imprimir este numero.*/
+/* se dispone una lista que contiene los examenes de grado
+finales para una clase de 20 alumnos.
+contar las calificaciones anakp de 65 e imprimir este numero*/
+
+
 #include<iostream>
 using namespace std;
 int main()
 {
-	
-	
-}
+        int cali =0,rep=0;
 
+
+                for(int i=0; i<20;i++)
+                {
+                cout<<"ingresa tu calificacion: "<<endl;
+                cin>>cali;
+                        if(cali<65 || cali==65)
+                        {
+                        rep++;
+                        }
+                }
+
+
+        cout<<" alumnos en peligro de reprobar : "<< rep<<" "<<endl;
+
+        return 0;
+
+}
